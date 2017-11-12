a02fe2670297360981e0881c30892c20fd4272ab
summer <810152690@qq.com>
2017-11-12 14:58:34 +0800

    first commit

---
+++ Classes/ZSUIUtility.h
@@ -0,0 +1,13 @@
+//
+//  ZSUIUtility.h
+//  IOSCommon
+//
+//  Created by summer on 2017/11/11.
+//  Copyright © 2017年 summer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZSUIUtility : NSObject
+
+@end
---
+++ Classes/ZSUIUtility.m
@@ -0,0 +1,13 @@
+//
+//  ZSUIUtility.m
+//  IOSCommon
+//
+//  Created by summer on 2017/11/11.
+//  Copyright © 2017年 summer. All rights reserved.
+//
+
+#import "ZSUIUtility.h"
+
+@implementation ZSUIUtility
+
+@end
